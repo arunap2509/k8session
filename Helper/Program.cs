b4a9f90c7a33778e49806c19a07af2042fc68070
Arun AP <arundhas7@gmail.com>
2025-04-21 17:13:02 +0530

    added configmaps and secret related yaml

--- Helper/Program.cs
+++ Helper/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.Configure<Config>(builder.Configuration);
 
 var app = builder.Build();
 
@@ -14,6 +17,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/name", () => "Arun");
+app.MapGet("/name", (IOptions<Config> options) =>
+{
+    var job = Environment.GetEnvironmentVariable("JOB_ENV_VARIABLE");
+    return $"{options.Value.Name} and i am an {job}";
+});
 
 app.Run();
+
+internal class Config
+{
+    public string Name { get; set; } = "";
+    public string Job { get; set; } = "";
+}
